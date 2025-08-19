2d7b778db1025fa78d594c13d3a5b21e4fe8eb98
Prasanna <prasannhanchinamani.15@gmail.com>
2025-08-19 15:26:49 +0530

    insert at specific index

--- src/basic_linkedList/SinglularLinkedList.java
+++ src/basic_linkedList/SinglularLinkedList.java
@@ -75,6 +75,25 @@ class LinkedList {
         temp.next = prev;
         return createLinkedListThroughRecursionStart(arr, index + 1, size, temp);
     }
+
+    public void insertAt(int index, int value) {
+        Node newNode = new Node(value);
+        if (head == null) {
+            newNode.next = head;
+            head = newNode;
+        }
+        Node curr = head;
+        int count = 0;
+        while (curr != null && count < index - 1) {
+            curr = curr.next;
+            count++;
+        }
+        if (curr == null)
+            System.out.println("Index Bound nt in range..");
+
+        newNode.next = curr.next;
+        curr.next = newNode;
+    }
 }
 
 public class SinglularLinkedList {
@@ -93,7 +112,9 @@ public class SinglularLinkedList {
         //head i will get on that
         linkedList.head = linkedList.createLinkedListThroughRecursion(arr, 0, arr.length);
         linkedList.displaylist();
-        linkedList.head=linkedList.createLinkedListThroughRecursionStart(arr,0,arr.length,null);
+        linkedList.head = linkedList.createLinkedListThroughRecursionStart(arr, 0, arr.length, null);
+        linkedList.displaylist();
+        linkedList.insertAt(3,55);
         linkedList.displaylist();
 
 
